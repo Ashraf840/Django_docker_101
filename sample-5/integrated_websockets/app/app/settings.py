83ce32a818f622b0684dcd26c36faa82c6e39f90
Ashraf840 <shift2unleashed2011@gmail.com>
2024-01-16 21:47:20 +0600

    Sample-5: Integrate uwsgi to serve dj app & extended NGINX proxy service into the docker-compose package

--- sample-5/integrated_websockets/app/app/settings.py
+++ sample-5/integrated_websockets/app/app/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v71&ywpoeuv5kr=^1i1@f(465oakj%xgfzli)wz5mpfu1ifh0f"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changemeDevServer')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS_ENV = os.environ.get('ALLOWED_HOSTS')
+if ALLOWED_HOSTS_ENV:
+    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
 
 
 # Application definition
@@ -143,7 +146,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/static/"
+MEDIA_URL = "/static/media/"
+
+STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = '/vol/web/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- sample-5/integrated_websockets/scripts/script.sh
+++ sample-5/integrated_websockets/scripts/script.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-set -e
-
 python manage.py collectstatic --noinput
-
 uwsgi --socket :8080 --master --enable-threads --module app.wsgi
