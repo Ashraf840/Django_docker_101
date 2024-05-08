9988c3b62560435004887a134e8e35534c3588de
Ashraf840 <shift2unleashed2011@gmail.com>
2024-05-08 18:08:28 +0600

    Added a person name table to display the names;

--- dj-docker-dockerCompose-1/src/personApp/views.py
+++ dj-docker-dockerCompose-1/src/personApp/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 from .models import Person
 
 class PersonPageView(View):
     def get(self, request):
-        return render(request, 'personApp/index.html')
+        persons = Person.objects.all()
+        context = {'persons':persons}
+        return render(request, 'personApp/index.html', context)
     
     def post(self, request):
         Person.objects.create(name=request.POST.get("name"))
-        return render(request, 'personApp/index.html')
+        return redirect('personApp:PersonPageView')
