539f6a93de979b391dab146198a21badba84c348
Ashraf840 <shift2unleashed2011@gmail.com>
2024-05-08 17:46:58 +0600

    Added a model "Person" to store the names persistently;

--- dj-docker-dockerCompose-1/src/personApp/views.py
+++ dj-docker-dockerCompose-1/src/personApp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-
+from .models import Person
 
 class PersonPageView(View):
     def get(self, request):
         return render(request, 'personApp/index.html')
     
     def post(self, request):
-        print("data:", request.POST.get("name"))
+        Person.objects.create(name=request.POST.get("name"))
         return render(request, 'personApp/index.html')
