0a4e3ebb807249b5d4b25c472b8475de159e3d6d
Ashraf840 <shift2unleashed2011@gmail.com>
2024-05-08 19:11:45 +0600

    Added a new field "profile_pic" store images; Relevant configuration has made;

--- dj-docker-dockerCompose-1/src/personApp/models.py
+++ dj-docker-dockerCompose-1/src/personApp/models.py
@@ -2,3 +2,4 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=100)
+    profile_pic = models.ImageField(upload_to='profilePicture', default='profilePicture/default_avatar.png', blank=True)
